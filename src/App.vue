db8063b60b7ab636edc2464241fdc7e4b8c94d56
song_my <song_my@example.com>
2018-07-22 13:44:40 +0800

    sdd

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+      hello world
+  </div>
+</template>
+<script type="text/esmascript-6">
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable";
+  #app
+    color: @color-theme
+</style>
